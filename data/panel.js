868e71a61d98e09813e5b5dc34255ae8c5835891
Nikolay Bryskin <nbryskin@yandex-team.ru>
2011-08-17 23:28:40 +0400

    buggy version with main functionality

--- data/panel.js
+++ data/panel.js
@@ -1,3 +1,25 @@
-$("input").click(function() {
-    self.port.emit("providerSelected", $(this).prop("id"));
-})
+function setHandlers() {
+    $("input").unbind();
+    $("input").click(function() {
+        self.port.emit("providerSelected", $(this).prop("id"));
+        return true;
+    });
+}
+
+setHandlers();
+
+self.port.on("addProvider", function(provider) {
+    console.log("addProvider(" + provider.id + ", " + provider.name + ")");
+    $("#providers").append("<input type=radio name='group1' id='" + provider.id + "'>" + provider.name + "</input><br>");
+    setHandlers();
+});
+
+self.port.on("selectProvider", function(provider) {
+    console.log("selectProvider(" + provider + ")");
+    $("#" + provider).prop("checked", true);
+});
+
+self.port.on("updateTables", function(info) {
+    console.log("updateTables(" + info + ")");
+    $("#tables").text(info);
+});
